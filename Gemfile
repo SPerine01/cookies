9397736cfdd3e41d8c56fc1f385e3f1351636468
Sumayyah Perine <sperine01@gmail.com>
2015-11-08 23:42:16 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,1 @@
+gem "paperclip", "~> 4.2"
---
+++ cookieshomework.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	// if(location.search){
+	// 	params = location.search.split("?") [1];
+	// 	value = params.split("=")[1];
+	// 	Cookies.set("value_submitted", value);
+	// 	}
+
+	// if(Cookies.get("value_submitted")) {
+	// 	$("#hip-hop").html(Cookies.get("value_submitted"));
+	// }
+
+	$("#submit").click(function() {
+		$("#form").hide();
+		// $("#hip-hop").slideToggle(2500);
+	});
+
+	// $("#submit-button").onclick(function () {
+	// $("#hip-hop").fadeIn(3000);
+	// });
+
+});
